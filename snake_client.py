98f02161b769cc4c2531cf0660d7bb10afaa0b36
milkybird98 <dengtimmycn@gmail.com>
2019-10-13 15:53:18 +0800

    init commit

---
+++ snake_client.py
@@ -0,0 +1,7 @@
+
+def game_init()
+  model_reset_map()
+  model_reset_players()
+  model_reset_snakes()
+
+  view_render()
---
+++ snake_server.py
@@ -0,0 +1,203 @@
+from flask import Flask,redirect, url_for, request,session,jsonify
+from datetime import timedelta
+import uuid
+from threading import Timer
+import os
+import random
+
+app = Flask(__name__)
+app.config['SECRET_KEY']=os.urandom(24)
+room_list=[]
+
+def proc_room_1s():
+   for room in room_list:
+      room['started'] = room['started']+1
+      if room['started'] > 600:
+         close_room(room)
+      else:
+         grow_apples(room)
+   Timer(1,proc_room_1s).start()
+
+
+def close_room_exe(room):
+   room.clear()
+   room = None
+
+def check_eat(room,user):
+   if room['map']['apples'].count(user['snake']['head']) == 1:
+      room['map']['apples'] = [apple for apple in room['map']['apples'] if not apple == user['snake']['head']]
+      user['snake']['len'] += 1
+      return True
+   else:
+      return False
+
+def check_crash(room,user):
+   if room['map']['walls'].count(user['snake']['head']) == 1:
+      user['snake']['len'] = -1
+   
+   for other_player in user['player']:
+      if other_player['snake']['body'].count(user['snake']['head']) == 1:
+         user['snake']['len'] = -1
+         break         
+            
+   for node in user['snake']['body']:
+      if room['map']['walls'].count(node) == 1:
+         user['snake']['len'] = -1
+         break
+
+   if user['snake']['len'] == -1:
+      if room['map']['walls'].count(user['snake']['head']) == 0:
+         room['map']['apples'].append(user['snake']['head'])
+      for node in user['snake']['body']:
+         if room['map']['walls'].count(node) == 0:
+            room['map']['apples'].append(node)
+      return True
+   else:
+      return False
+   
+def grow_apples(room):
+   while(1):
+      x = random.randint(0,99)
+      y = random.randint(0,99)
+      if room['map']['walls'].count({'x':x,'y':y}) == 0:
+         for player in room['player']:
+            if player['snake']['body'].count({'x':x,'y':y}) > 0:
+               continue
+         room['map']['apples'].append({'x':x,'y':y})
+         break
+      continue
+   return
+
+def close_room(room):
+   room['started'] = 0
+   Timer(10,close_room_exe).start()
+
+def start_room(user):
+   room = {}
+   room['num_player'] = 1
+   room['started'] = 1
+   room['player'] = [user]
+   room['map']={}
+   room['map']['apples']=[]
+   room['map']['walls']=[]
+   room_list.append(room)
+   room_index = room_list.index(room)
+   return room_index
+
+def join_room(room,user):
+   room['num_player'] = room['num_player']+1
+   room['players'].append(user)
+
+def join_game(user):
+   for room in room_list:
+      if room.get('started') > 0:
+         if room.get('num_player') <= 8:
+            join_room(room,user)
+            return room_list.index(room)
+   else:
+      room_index = start_room(user)
+      return room_index
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=10)
+
+@app.route('/join/<name>')
+def reg_user(name):
+   session['uuid'] = uuid.uuid1()
+   user = {'uuid':session['uuid'],'name':name,'score':0,'snake':{'body':[],'len':3,'head':{}}}
+   room = join_game(user)
+   session['room'] = room 
+   return 'success'
+
+@app.route('/update/snake',methods = ['POST'])
+def update_snake():
+   room = room_list[session.get('room')]
+   if room != None: 
+      snake_body = eval(request.form['body'])
+      snake_head = eval(request.form['head'])
+
+      user = [user for user in room['player'] if user['uuid'] == session['uuid']][0]
+      user['snake']['body'] = snake_body
+      user['snake']['head'] = snake_head
+
+      if check_crash(room,user):
+         return 'die'
+      elif check_eat(room,user):
+         return 'eat'
+      else:
+         return 'success'
+   else:
+      return 'over'
+   
+@app.route('/update/wall',methods = ['POST'])
+def update_wall():
+   room = room_list[session.get('room')]
+   if room != None: 
+      wall = eval(request.form['wall'])
+      room['map']['walls'].append({'x':wall['x'],'y':wall['y']})
+      room['map']['apples'].remove({'x':wall['x'],'y':wall['y']})
+      return 'success'
+   else:
+      return 'over'
+
+@app.route('/get/status')
+def get_status():
+   game_status={}
+   room = room_list[session.get('room')]
+   if room != None: 
+      game_status['time'] = room['started']
+      game_status['count'] = len(room['player'])
+      game_status['user'] = [user for user in room['player'] if user['uuid'] == session['uuid']][0]
+      return jsonify(game_status)
+   else:
+      return 'over'
+
+@app.route('/get/snakes')
+def get_snakes():
+   snakes=[]
+   room = room_list[session.get('room')]
+   if room != None:
+      for user in room:
+         snakes.append({'name':user['name'],
+                        'body':user['snake']['body'],
+                        'len':user['snake']['len'],
+                        'head':user['snake']['head']})
+      return jsonify(snakes)
+   else:
+      return 'over'
+
+@app.route('/get/scores')
+def get_scores():
+   scores=[]
+   room = room_list[session.get('room')]
+   if room != None:
+      for user in room['player']:
+         scores.append({'name':user['name'],
+                        'score':user['score']})
+      return jsonify(scores)
+   else:
+      return 'over'
+
+@app.route('/get/walls')
+def get_walls():
+   room = room_list[session.get('room')]
+   if room != None:
+      return jsonify(room['map']['walls'])
+   else:
+       return 'over'
+
+@app.route('/get/apples')
+def get_apples():
+   room = room_list[session.get('room')]
+   print(room)
+   if room != None:
+      return jsonify(room['map']['apples'])
+   else:
+      return 'over'
+
+if __name__ == '__main__':
+   Timer(1,proc_room_1s).start()
+   app.run(debug=True)
+   
